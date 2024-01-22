bc5c51ef69a0873a20aeff527002da92b217a908
Colton923 <110353948+Colton923@users.noreply.github.com>
2024-01-22 10:08:33 -0600

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,61 @@
 import * as stylex from '@stylexjs/stylex'
 import '@/styles/globals.css'
 import AppShell from '@/components/AppShell/AppShell'
+import { Metadata } from 'next'
 
 interface Props {
   children: React.ReactNode
   modal: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://mcclintocktruckingandexcavating.com'),
+  title: 'McClintock Trucking & Excavating',
+  description: 'Quad Cities, Illinois, and Iowa - Trucking & Excavating',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: 'favicon.ico',
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+    other: [
+      {
+        rel: 'favicon-32x32',
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
+        rel: 'favicon-16x16',
+        url: '/favicon-16x16.png',
+        sizes: '16x16',
+      },
+      {
+        rel: 'manifest',
+        url: '/site.webmanifest',
+      },
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://mcclintocktruckingandexcavating.com',
+    title: 'McClintock Trucking & Excavating',
+    description: 'Quad Cities Area Trucking and Excavating',
+    locale: 'en_US',
+    images: [
+      {
+        url: 'https://mcclintocktruckingandexcavating.comm/excavator-32.png',
+        width: 32,
+        height: 32,
+        alt: 'McClintock Trucking & Excavating',
+      },
+    ],
+  },
 }
 
 export default async function RootLayout({ children, modal }: Props) {
@@ -16,7 +67,11 @@ export default async function RootLayout({ children, modal }: Props) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-        <meta name="keywords" content="Liberty Village" />
+        <meta name="keywords" content="McClintock Trucking & Excavating" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body {...stylex.props(styles.reset, styles.body)}>
         <main {...stylex.props(styles.main)}>
